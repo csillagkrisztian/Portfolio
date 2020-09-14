35d47de0022ec5278008e3296ac151016137fd90
csillagkrisztian <krisztian.csillag93@gmail.com>
2020-09-14 17:45:55 +0200

    added a working dynamic page

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,6 +1,30 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql } from "gatsby"
 
-export default function Blog() {
-  return <Layout></Layout>
+// I think this $slug is equal to context.slug and that is how when the page is created, it gets the info by accessing props.data
+
+export const query = graphql`
+  query($slug: String) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`
+
+export default function Blog(props) {
+  // this will be exactly the markdown file I was looking for
+  const { markdownRemark } = props.data
+  console.log(props.data)
+  return (
+    <Layout>
+      <h2>{markdownRemark.frontmatter.title}</h2>
+      <p>{markdownRemark.frontmatter.date}</p>
+      <div dangerouslySetInnerHTML={{ __html: markdownRemark.html }}></div>
+    </Layout>
+  )
 }
